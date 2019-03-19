5d7e32089cd0a3b708f027c8f53bbf861ecda3bf
German Varanytsya <s12943@pjwstk.edu.pl>
2019-03-19 16:00:48 +0200

    HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,21 @@
 </template>
 
 <style lang="scss">
+  html{
+    background-color: black;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: white;
     &.router-link-exact-active {
       color: #42b983;
     }
