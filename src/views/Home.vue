e6bea95069fc1994f11a88b56b1a9fbdb0504efa
German Varanytsya <s12943@pjwstk.edu.pl>
2019-03-19 12:32:14 +0200

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RandImg />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import RandImg from '@/components/RandImg.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    RandImg
+  }
+}
+</script>
