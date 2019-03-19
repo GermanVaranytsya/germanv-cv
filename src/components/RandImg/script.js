7a0cffaa1bff9341dfe300e9b343504d22ee8a7c
German Varanytsya <s12943@pjwstk.edu.pl>
2019-03-19 16:56:57 +0200

    refactor moveImage()

--- src/components/RandImg/script.js
+++ src/components/RandImg/script.js
@@ -1,33 +1,34 @@
 const vue = require("@/assets/images/vue.png");
-  export default {
-    name: "randImg",
-    data() {
+export default {
+  name: "randImg",
+  data() {
+    return {
+      vue,
+      imgTop: 0,
+      imgLeft: 0,
+      imgHeight: 64,
+      imgWidth: 64,
+      changeInterval: 1000
+    }
+  },
+  computed: {
+    imgStyle() {
       return {
-        vue,
-        imgTop: 0,
-        imgLeft: 0,
-        imgHeight: 64,
-        imgWidth: 64,
-        changeInterval: 1000
-      }
-    },
-    computed: {
-      imgStyle() {
-          return {
-            top: `${this.imgTop}px`,
-            left: `${this.imgLeft}px`,
-            height: `${this.imgHeight}px`,
-            width: `${this.imgWidth}px`
-          }
-        }
-    },
-    created() {
-      setInterval(this.moveImage, this.changeInterval);
-    },
-    methods: {
-      moveImage() {
-        this.imgTop = Math.round(Math.random() * (window.innerHeight - this.imgHeight));
-        this.imgLeft = Math.round(Math.random() * (window.innerWidth - this.imgWidth));
+        top: `${this.imgTop}px`,
+        left: `${this.imgLeft}px`,
+        height: `${this.imgHeight}px`,
+        width: `${this.imgWidth}px`
       }
     }
+  },
+  created() {
+    setInterval(this.moveImage, this.changeInterval);
+  },
+  methods: {
+    moveImage() {
+      let random = twoSizes => Math.round(Math.random() * twoSizes);
+      this.imgTop = random(screen.height - this.imgHeight);
+      this.imgLeft = random(screen.width - this.imgWidth);
+    }
   }
+}
