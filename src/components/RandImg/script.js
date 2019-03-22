25e01c43a3c5172a394b5448551e66b66e0045b2
German Varanytsya <s12943@pjwstk.edu.pl>
2019-03-22 13:27:11 +0200

    random img added

--- src/components/RandImg/script.js
+++ src/components/RandImg/script.js
@@ -1,14 +1,34 @@
 const vue = require("@/assets/images/vue.png");
+const bootstrap = require("@/assets/images/bootstrap.png");
+const bulma = require("@/assets/images/bulma.png");
+const css3 = require("@/assets/images/css3.png");
+const html5 = require("@/assets/images/html5.png");
+const illustrator = require("@/assets/images/illustrator.png");
+const js = require("@/assets/images/js.png");
+const photoshop = require("@/assets/images/photoshop.png");
+const webpack = require("@/assets/images/webpack.png");
+
 export default {
   name: "randImg",
   data() {
     return {
-      vue,
-      imgTop: 0,
-      imgLeft: 0,
+      images: [
+        vue,
+        bootstrap,
+        bulma,
+        css3,
+        html5,
+        illustrator,
+        js,
+        photoshop,
+        webpack
+      ],
+      imgTop: -100,
+      imgLeft: -100,
       imgHeight: 64,
       imgWidth: 64,
-      changeInterval: 1000
+      changeInterval: 1000,
+      selectedImage: ''
     }
   },
   computed: {
@@ -22,13 +42,19 @@ export default {
     }
   },
   created() {
-    setInterval(this.moveImage, this.changeInterval);
+    const randomImg = func => setInterval(func, this.changeInterval);
+    randomImg(this.randomImage);
+    randomImg(this.moveImage);
   },
   methods: {
+    randomImage() {
+      const idx = Math.floor(Math.random() * this.images.length);
+      this.selectedImage = this.images[idx];
+    },
     moveImage() {
-      let random = twoSizes => Math.round(Math.random() * twoSizes);
-      this.imgTop = random(window.innerHeight - this.imgHeight);
-      this.imgLeft = random(window.innerWidth - this.imgWidth);
+      const randomPos = twoSizes => Math.round(Math.random() * twoSizes);
+      this.imgTop = randomPos(window.innerHeight - this.imgHeight);
+      this.imgLeft = randomPos(window.innerWidth - this.imgWidth);
     }
   }
 }
